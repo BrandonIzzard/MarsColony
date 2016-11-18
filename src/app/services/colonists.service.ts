071c5381a2912f2cfd86c2faeed962fe02d676f6
Brandon Izzard <xbrandoni@gmail.com>
2016-11-17 17:06:37 -0800

    starting colonist local storage

--- src/app/services/colonists.service.ts
+++ src/app/services/colonists.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import { Colonist, NewColonist } from '../models';
+
 
 @Injectable()
-export class ColonistsService {
+export default class ColonistService {
 
-  constructor() { }
+
+	COLONIST_JSON = 'https://red-wdp-api.herokuapp.com/api/mars/colonist';
+
+
+	constructor(private http: Http) { }
+
+	getColonist(): Observable<Colonist[]> {
+		return this.http.get(this.COLONIST_JSON)
+						.map((res: Response) => res.json().colonist);
+          
+	}
+  submitColonist(colonist: NewColonist): Observable<Colonist> {
+		let headers = new Headers({ 'Content-Type': 'application/json' });
+		let options = new RequestOptions({ headers: headers });
+
+		return this.http.post(this.COLONIST_JSON, { colonist }, { headers })
+			.map((res: Response) => res.json().colonist);
 
 }
